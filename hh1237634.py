5e8b1ae29ba823ef1f050b56be5404f1fecee145
Alek <101995970+XHackerFinnX@users.noreply.github.com>
2022-06-08 13:52:23 +0300

    Create hh1237634.py

---
+++ hh1237634.py
@@ -0,0 +1,28 @@
+def is_password_good(password):
+    sum = 0
+    if len(password) >= 8:
+        sum += 1
+        for i in range(len(password)):
+            if password[i] in '0123456789':
+                continue
+            if password.upper()[i] in password[i]:
+                sum += 1
+                break
+        else:
+            return False
+        for i in range(len(password)):
+            if password.lower()[i] in password[i]:
+                sum += 1
+                break
+        else:
+            return False
+        for i in range(len(password)):
+            if password[i] in '0123456789':
+                sum += 1
+                return True
+    else:
+        return False
+
+txt = input()
+
+print(is_password_good(txt))
